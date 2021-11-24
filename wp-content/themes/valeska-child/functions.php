63f48ffed90efe0435cd24f03d034f41719ede7a
riscaldo <riccardo85ferrara@gmail.com>
2021-11-24 17:40:13 +0100

    alert selected color

--- wp-content/themes/valeska-child/functions.php
+++ wp-content/themes/valeska-child/functions.php
@@ -23,13 +23,54 @@ add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 25 );
 
 // END ENQUEUE PARENT ACTION
 
+
+// ----------------------------------------------------------------------------//
+//                RIDEV                                                        //
+// ----------------------------------------------------------------------------//
+
+//here some test 
 add_action('woocommerce_before_single_product','print_color');
 function print_color(){
-    echo '<h2>green</h2>';
-
+    echo '<h2>blue</h2>';
 }
 
+//here some test
 add_action( 'woocommerce_before_single_product', 'quadlayers_woocommerce_hooks');
 function quadlayers_woocommerce_hooks() {
 echo '<img src="https://kokohai.com/wp-content/uploads/2020/02/logo-kokohai-tienda-de-merchandising-de-anime-y-maga-e1584570981420.png">'; // Change to desired image url
 }
+
+
+//hook javascript per lettura colori nella single-product page
+add_action('woocommerce_after_single_variation','custom_jquery_shop_script');
+//funzione che aggiunge javascript per la lettura dei colori
+function custom_jquery_shop_script(){
+        ?>
+            <script type="text/javascript">
+            var colors
+
+            //inserisci delle funzioni click per ogni colore
+            function assignOnClickBehavoir(colors){
+                for (var i=0, iLen=colors.length; i<iLen; i++) {
+                    colors[i].onclick = function() {
+                        alert(this.dataset.value)
+                    }
+                }
+            }
+
+            //non appena l'elemento colors viene caricato sulla pagina viene generata la variabile colors
+            function waitForColorsElements(){
+                if(typeof document.querySelectorAll("form.variations_form.cart.wvs-loaded [name='color']")[0] !== "undefined"){
+                    //l'elemento esiste sulla pagina ora carichiamola in "colors"
+                    colors = document.querySelectorAll("form.variations_form.cart.wvs-loaded [name='color']")
+                    assignOnClickBehavoir(colors)
+                }
+                else{
+                    setTimeout(waitForColorsElements, 250);
+                }
+            }
+            waitForColorsElements()
+            </script>
+        <?php
+}
+
