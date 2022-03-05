cd41e2261a144afc139c3714c6b903237c1fdc5a
riscaldo <riccardo85ferrara@gmail.com>
2022-03-05 16:47:05 +0100

    no need for these functions

---
+++ wp-content/themes/valeska-child/assets/js/frontend/pag-numbers.js
@@ -0,0 +1,7 @@
+// next = document.querySelectorAll('.next')
+// Object.entries(next).map(s => s[1].classList.remove('next'))
+// Object.entries(next).map(s => s[1].classList.add('no-r-border'))
+
+
+// prev = document.querySelectorAll('.prev')
+// Object.entries(prev).map(s => s[1].classList.remove('prev'))
