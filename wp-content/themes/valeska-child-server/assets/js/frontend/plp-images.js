51fe41dd0c7f298b8e67c20643500998a4849088
riscaldo <riccardo85ferrara@gmail.com>
2022-03-09 15:34:29 +0100

    plp imgs are not cropped

---
+++ wp-content/themes/valeska-child-server/assets/js/frontend/plp-images.js
@@ -0,0 +1,6 @@
+var imgs = document.querySelectorAll('.attachment-woocommerce_thumbnail.size-woocommerce_thumbnail')
+Object.entries(imgs).map(img => {
+    console.log(img[1])
+    img[1].sizes = "(max-width: 300px)"
+    img[1].srcset = img[1].src.split('-').slice(0, -1).join('-') + '.' + img[1].src.split('.').pop()
+})
