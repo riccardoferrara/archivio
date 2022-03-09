07c5c3c15b9814d4473a810c0f1e6eb70a44c659
riscaldo <riccardo85ferrara@gmail.com>
2022-03-09 20:04:17 +0100

    fix bug in foulard: foulard had no size

--- wp-content/themes/valeska-child-server/functions.php
+++ wp-content/themes/valeska-child-server/functions.php
@@ -71,11 +71,15 @@ function custom_jquery_shop_script(){
             // if page is loaded from query, color is in the address
             selected_color = window.location.href.split('/').slice(-1)[0].split(/[=?&]/)[2]
 
+            let no_size = true
+
             if ( document.querySelectorAll('[for="pa_size"]')[0]){
                 document.querySelectorAll('[for="pa_size"]')[0].innerText = 'SIZE'
+                no_size = false
             }
             if (document.querySelectorAll('[for="size"]')[0]){
                 document.querySelectorAll('[for="size"]')[0].innerText = 'SIZE'
+                no_size = false
             }
             if (document.querySelectorAll('[for="pa_color"]')[0]) {
                 document.querySelectorAll('[for="pa_color"]')[0].innerText = 'COLOR'
@@ -84,20 +88,35 @@ function custom_jquery_shop_script(){
             //muovi le etichette COLOR and SIZE da accanto a sopra nelle tabelle
             //recupera la tabella
             let var_table = document.querySelector('table.variations')
+
+            //color:
             //recupera la cella contenente il colore e la taglia per ricopiarla dopo
             var color_th = var_table.childNodes[1].childNodes[1].childNodes[1]
-            var size_th = var_table.childNodes[1].childNodes[3].childNodes[1]
             // rimuovi vecchie etichette
             var_table.childNodes[1].childNodes[1].deleteCell(0)
-            var_table.childNodes[1].childNodes[3].deleteCell(0)
             // inserisci due nuove righe
             var color_row = var_table.insertRow(0)
-            var size_row = var_table.insertRow(2)
             // inserisci una cella per riga
             var color = color_row.insertCell(0)
-            var size = size_row.insertCell(0)
             color.outerHTML = color_th.outerHTML
-            size.outerHTML = size_th.outerHTML
+
+            //size:
+            if (!no_size){
+                //recupera la cella contenente il colore e la taglia per ricopiarla dopo
+                var size_th = var_table.childNodes[1].childNodes[3].childNodes[1]
+                // rimuovi vecchie etichette
+                var_table.childNodes[1].childNodes[3].deleteCell(0)
+                // inserisci due nuove righe
+                var size_row = var_table.insertRow(2)
+                // inserisci una cella per riga
+                var size = size_row.insertCell(0)
+                size.outerHTML = size_th.outerHTML
+            }
+
+
+
+
+
 
 
             //inserisci delle funzioni click per ogni colore
