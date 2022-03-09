eea6072e95acc28643dd17b821a7fccde31d6a4c
riscaldo <riccardo85ferrara@gmail.com>
2022-03-09 20:39:51 +0100

    plp uses compressed imgs

--- wp-content/themes/valeska-child-server/assets/js/frontend/plp-images.js
+++ wp-content/themes/valeska-child-server/assets/js/frontend/plp-images.js
@@ -2,5 +2,5 @@ var imgs = document.querySelectorAll('.attachment-woocommerce_thumbnail.size-woo
 Object.entries(imgs).map(img => {
     console.log(img[1])
     img[1].sizes = "(max-width: 300px)"
-    img[1].srcset = img[1].src.split('-').slice(0, -1).join('-') + '.' + img[1].src.split('.').pop()
+    img[1].srcset = img[1].src.split('-').slice(0, -1).join('-') + '-768x1024' + '.' + img[1].src.split('.').pop()
 })
