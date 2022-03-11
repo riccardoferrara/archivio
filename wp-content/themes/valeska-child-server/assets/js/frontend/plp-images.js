e3d699ff4fb70d23b357b05a559563e3a4d76afe
riscaldo <riccardo85ferrara@gmail.com>
2022-03-11 20:28:57 +0100

    nothing changes: prepare to add links to prices

--- wp-content/themes/valeska-child-server/assets/js/frontend/plp-images.js
+++ wp-content/themes/valeska-child-server/assets/js/frontend/plp-images.js
@@ -1,6 +1,17 @@
-var imgs = document.querySelectorAll('.attachment-woocommerce_thumbnail.size-woocommerce_thumbnail')
-Object.entries(imgs).map(img => {
-    console.log(img[1])
-    img[1].sizes = "(max-width: 300px)"
-    img[1].srcset = img[1].src.split('-').slice(0, -1).join('-') + '-768x1024' + '.' + img[1].src.split('.').pop()
-})
+// change imgs resolution to load page faster
+function changeImgsResolution() {
+    var imgs = document.querySelectorAll('.attachment-woocommerce_thumbnail.size-woocommerce_thumbnail')
+    Object.entries(imgs).map(img => {
+        // console.log(img[1])
+        img[1].sizes = "(max-width: 300px)"
+        img[1].srcset = img[1].src.split('-').slice(0, -1).join('-') + '-768x1024' + '.' + img[1].src.split('.').pop()
+    })
+}
+
+// add link to prices that links to the pdp page
+function addLinkToPrices() {
+    var prices = document.querySelectorAll('.woocommerce-Price-amount.amount')
+}
+
+
+changeImgsResolution()
