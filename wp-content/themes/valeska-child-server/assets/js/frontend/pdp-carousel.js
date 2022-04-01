99d17fe514b40b8d4cfbc1d5b558cd385d651df2
riscaldo <riccardo85ferrara@gmail.com>
2022-04-01 12:55:32 +0200

    first img of the carousel is the still

--- wp-content/themes/valeska-child-server/assets/js/frontend/pdp-carousel.js
+++ wp-content/themes/valeska-child-server/assets/js/frontend/pdp-carousel.js
@@ -1,8 +1,42 @@
-// the still image will be the first
-function setFirstImgOfCarousel() {
-    document.querySelector('.elementor-image-carousel.swiper-wrapper').style.transform = "translate3d(-650px, 0px, 0px)"
+window.addEventListener('load', (event) => {
+    setStillImgAsFirst()
+});
+
+
+//among the carousel imgs it gives the index of the still img
+function getIndexOfStillImg() {
+    var imgs = document.querySelectorAll('.swiper-slide.img-mobile .swiper-slide-image')
+    for (let i = 0; i < imgs.length; i++) {
+        if (imgs[i].src.split('/').slice(-1)[0].split('-')[2] == '0') {
+            return i
+        }
+    }
 }
 
-window.addEventListener('load', (event) => {
-    setFirstImgOfCarousel()
-});
+//among the carousel imgs it gives the index of the active img
+function getIndexOfActiveImg() {
+    var imgs = document.querySelectorAll('.swiper-slide.img-mobile')
+    for (let i = 0; i < imgs.length; i++) {
+        if (imgs[i].classList.contains('swiper-slide-active')) {
+            return i
+        }
+    }
+}
+
+//reset the still img as the first of the carousel
+function setStillImgAsFirst() {
+    let i_still = getIndexOfStillImg()
+    let i_active = getIndexOfActiveImg()
+    console.log('i_still: ', i_still)
+    console.log('i_active: ', i_active)
+    var imgs_front = document.querySelectorAll('.swiper-slide.img-mobile .swiper-slide-image')
+    var imgs_copy = [...imgs_front]
+    for (let i = 0; i <= 5; i++) {
+        console.log('i_src: ', (i + i_still) % 6)
+        console.log('src: ', imgs_copy[(i + i_still) % 6].src)
+        console.log('dst: ', imgs_front[(i + i_active) % 6].src)
+        console.log('i dest:, ', (i + i_active) % 6)
+        imgs_front[(i + 1) % 6].src = imgs_copy[(i + i_still) % 6].src
+        console.log('\n')
+    }
+}
