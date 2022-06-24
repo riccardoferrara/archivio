cc7fda29167a07f6ee661f5648974cd7b43dda37
riscaldo <riccardo85ferrara@gmail.com>
2022-06-24 12:08:40 +0700

    random order of images in plp

--- wp-content/themes/valeska-child-server/functions.php
+++ wp-content/themes/valeska-child-server/functions.php
@@ -31,6 +31,31 @@ add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 25 );
 //                RIDEV                                                        //
 // ----------------------------------------------------------------------------//
 //******************************************************************************/
+//---------------------------------------------
+// RANDOM ORDER IN PLP
+//---------------------------------------------
+
+add_filter( 'woocommerce_get_catalog_ordering_args', 'custom_woocommerce_get_catalog_ordering_args' );
+ 
+function custom_woocommerce_get_catalog_ordering_args( $args ) {
+$orderby_value = isset( $_GET['orderby'] ) ? woocommerce_clean( $_GET['orderby'] ) : apply_filters( 'woocommerce_default_catalog_orderby', get_option( 'woocommerce_default_catalog_orderby' ) );
+ 
+if ( 'random_list' == $orderby_value ) {
+$args['orderby'] = 'rand';
+$args['order'] = '';
+$args['meta_key'] = '';
+}
+return $args;
+}
+ 
+add_filter( 'woocommerce_default_catalog_orderby_options', 'custom_woocommerce_catalog_orderby' );
+add_filter( 'woocommerce_catalog_orderby', 'custom_woocommerce_catalog_orderby' );
+ 
+function custom_woocommerce_catalog_orderby( $sortby ) {
+$sortby['random_list'] = 'Random';
+return $sortby;
+}
+
 
 //---------------------------------------------
 // DECLARE SUPPORT THEME FOR WOOCOMMERCE
